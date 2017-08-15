5716459045c74c09d7fe7fb65fc2788e79fbc914
Andy <29895539+andy-grandy@users.noreply.github.com>
2017-08-15 16:32:04 +0300

    Update real.py

--- real.py
+++ real.py
@@ -5,5 +5,5 @@ from bs4 import BeautifulSoup
 page = urlopen('http://www.realmadrid.com/en/tickets')
 soup = BeautifulSoup(page, 'html.parser')
 for i in soup.find_all('td', class_='m_match_aditional_info'):
-    if i.text == 'Matchday 15':
+    if i.text == 'Matchday 17':
         exit(1)
